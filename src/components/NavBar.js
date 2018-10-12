3080fb34c9b11e5f250d6902e9fa7bd5dffe31df
epilurzu <e.ipodda@gmail.com>
2018-10-12 17:44:00 +0200

    add margin styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -6,14 +8,21 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import BookIcon from '@material-ui/icons/Book';
 
+const styles = {
+    right: {
+        marginLeft: 'auto',
+        marginRight: -12,
+    }
+};
 
-const NavBar = () => {
+const NavBar = (props) => {
+    const { classes } = props;
     return(
         <div>
             <AppBar position="static">
                 <Toolbar>
 
-                    <IconButton color="inherit" aria-label="Menu">
+                    <IconButton color="inherit" aria-label="Menu" >
                         <MenuIcon />
                     </IconButton>
 
@@ -21,8 +30,8 @@ const NavBar = () => {
                         Pinella
                     </Typography>
 
-                    <IconButton color="inherit">
-                      <BookIcon/>
+                    <IconButton color="inherit" className={classes.right}>
+                        <BookIcon/>
                     </IconButton>
 
                 </Toolbar>
@@ -31,4 +40,8 @@ const NavBar = () => {
     )
 }
 
-export default NavBar;
+NavBar.propTypes = {
+    classes: PropTypes.element.isRequired
+};
+
+export default withStyles(styles)(NavBar);
